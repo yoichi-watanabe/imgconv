ad29222083e5721cdf214178850d93d228f371bb
yoichi-watanabe <chip.star1120@gmail.com>
2018-04-17 02:13:52 +0900

    指定ディレクトリ内の画像ファイルを再帰的に探す処理を実装

--- imgconv.go
+++ imgconv.go
@@ -4,58 +4,62 @@ import (
 	"flag"
 	"fmt"
 	"image"
-	"image/gif"
 	"image/jpeg"
 	"image/png"
 	"os"
 	"path/filepath"
 )
 
-func Convert() {
-	// 処理
+//Convert はオプションで指定された拡張子へ画像ファイルを変換する関数
+func Convert() error {
 	var (
-		//dir  = flag.String("dir", "./", "Message")
-		src  = flag.String("s", "jpg", "Message")
-		dest = flag.String("d", "png", "Message")
+		targetdir = flag.String("t", "./", "Message")
+		src       = flag.String("s", "jpg", "Message")
+		dest      = flag.String("d", "png", "Message")
 	)
 
 	// オプションのパース
 	flag.Parse()
 
-	path := "./testdata/sample1.jpg"
-	fmt.Println(filepath.Ext(path))
-
 	// 対象拡張子のファイルを抽出
-	if filepath.Ext(path) == fmt.Sprintf(".%s", *src) {
+	err := filepath.Walk(*targetdir, func(path string, info os.FileInfo, err error) error {
+		if filepath.Ext(path) == fmt.Sprintf(".%s", *src) {
+			// 処理
+			// 対象ファイルオープン
+			file, err := os.Open(path)
+			defer file.Close()
+			if err != nil {
+				fmt.Println(err)
+			}
 
-		// 対象ファイルオープン
-		file, err := os.Open(path)
-		defer file.Close()
-		if err != nil {
-			fmt.Println(err)
+			// デコード
+			img, _, err := image.Decode(file)
+			if err != nil {
+				fmt.Println(err)
+			}
+
+			fmt.Println(getFileNameWithoutExt(path) + fmt.Sprintf(".%s", *dest) + " を出力します")
+			out, err := os.Create(getFileNameWithoutExt(path) + fmt.Sprintf(".%s", *dest))
+			defer out.Close()
+
+			// 変換後拡張子に応じたエンコード
+			switch *dest {
+			case "jpg", "jpeg":
+				jpeg.Encode(out, img, nil)
+
+			case "png":
+				png.Encode(out, img)
+			}
+
+			fmt.Println(getFileNameWithoutExt(path) + fmt.Sprintf(".%s", *dest) + " を出力しました")
+
 		}
-
-		// デコード
-		img, _, err := image.Decode(file)
-		if err != nil {
-			fmt.Println(err)
-		}
-
-		out, err := os.Create(getFileNameWithoutExt(path) + fmt.Sprintf(".%s", *dest))
-		defer out.Close()
-
-		// 変換後拡張子に応じたエンコード
-		switch *dest {
-		case "jpg", "jpeg":
-			jpeg.Encode(out, img, nil)
-
-		case "png":
-			png.Encode(out, img)
-
-		case "gif":
-			gif.Encode(out, img, nil)
-		}
+		return nil
+	})
+	if err != nil {
+		return err
 	}
+	return nil
 }
 
 func getFileNameWithoutExt(path string) string {
