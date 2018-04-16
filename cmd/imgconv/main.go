87658955f00b8088fc6e59e3183e6d853e54ed3a
yoichi-watanabe <chip.star1120@gmail.com>
2018-04-17 00:14:53 +0900

    画像パス変換処理を実装

--- cmd/imgconv/main.go
+++ cmd/imgconv/main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
-    "flag"
+	"flag"
 	"fmt"
+	"image"
+	_ "image/jpeg"
+	"image/png"
 	"os"
+	"path/filepath"
 )
 
 // エンドポイント
@@ -11,21 +15,49 @@ func main() {
 	os.Exit(Run(os.Args))
 }
 
-
 // execute関数
 func Run(args []string) int {
 	// 処理
 	var (
-		dir   = flag.String("dir", "./", "Message")
-		from   = flag.String("from", "jpg", "Message")
-		to   = flag.String("to", "png", "Message")
-    )
+		dir  = flag.String("dir", "./", "Message")
+		src  = flag.String("src", ".jpg", "Message")
+		dest = flag.String("dest", "./testdata/dest.png", "Message")
+	)
 
-    flag.Parse()
+	// オプションのパース
+	flag.Parse()
+
+	path := "./testdata/sample1.jpg"
+	fmt.Println(filepath.Ext(path))
+
+	// 対象拡張子のファイルを抽出
+	if filepath.Ext(path) == *src {
+		fmt.Println(path)
+		fmt.Println("MATCH")
+
+		// 対象ファイルオープン
+		file, err := os.Open(path)
+		defer file.Close()
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		// デコード
+		img, _, err := image.Decode(file)
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		// エンコード
+		out, err := os.Create(*dest)
+		defer out.Close()
+
+		png.Encode(out, img)
+	}
 
 	fmt.Printf("param -dir -> %s\n", *dir)
-	fmt.Printf("from -from -> %s\n", *from)
-	fmt.Printf("to -to -> %s\n", *to)
-	
+	fmt.Printf("src -src -> %s\n", *src)
+	fmt.Printf("dits -dest -> %s\n", *dest)
+
 	return 0
 }
