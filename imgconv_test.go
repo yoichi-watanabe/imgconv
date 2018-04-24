4ca4f668a8120e491b2f8d2f321656dca373fdf0
yoichi-watanabe <chip.star1120@gmail.com>
2018-04-24 23:33:14 +0900

    fix code comment

--- imgconv_test.go
+++ imgconv_test.go
@@ -2,6 +2,7 @@ package imgconv
 
 import "testing"
 
+// getFileNameWithoutExt のテスト
 func TestGetFileNameWithoutExt(t *testing.T) {
 	if getFileNameWithoutExt("aaa.png") != "aaa" {
 		t.Fatal("getFileNameWithoutExt(\"aaa.png\") shold be \"aaa\", but doesn't match")
