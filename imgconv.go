96e6077a94588da4cc6381897967236e18f44a8c
yoichi-watanabe <chip.star1120@gmail.com>
2018-04-26 03:06:23 +0900

    テストが通らない

--- imgconv.go
+++ imgconv.go
@@ -14,28 +14,25 @@ import (
 
 // Cmdoption はコマンド実行時に設定されたオプションを持つ構造体
 type Cmdoption struct {
-	targetdir *string
-	src       *string
-	dest      *string
+	targetdir string
+	src       string
+	dest      string
 }
 
 //Convert はオプションで指定された拡張子へ画像ファイルを変換する関数
 func Convert(args []string) error {
 
-	c := Cmdoption{
-		flag.String("t", "./", "Message"),
-		flag.String("s", "jpg", "Message"),
-		flag.String("d", "png", "Message")}
-
 	// オプションのパース
-	flag.Parse()
+	c := parseFlag(args)
+	fmt.Println("parseFlagから返されたCの値")
+	fmt.Println(c.dest)
 
 	// ログ出力
-	fmt.Println(fmt.Sprintf("%s配下の拡張子を変換します。 %s → %s", *c.targetdir, *c.src, *c.dest))
+	fmt.Println(fmt.Sprintf("%s配下の拡張子を変換します。 %s → %s", c.targetdir, c.src, c.dest))
 
 	// 対象拡張子のファイルを抽出
-	err := filepath.Walk(*c.targetdir, func(path string, info os.FileInfo, err error) error {
-		if filepath.Ext(path) == fmt.Sprintf(".%s", *c.src) {
+	err := filepath.Walk(c.targetdir, func(path string, info os.FileInfo, err error) error {
+		if filepath.Ext(path) == fmt.Sprintf(".%s", c.src) {
 			// 対象ファイルオープン
 			file, err := os.Open(path)
 			defer file.Close()
@@ -49,12 +46,12 @@ func Convert(args []string) error {
 				fmt.Println(err)
 			}
 
-			fmt.Println(getFileNameWithoutExt(path) + fmt.Sprintf(".%s", *c.dest) + " を出力します")
-			out, err := os.Create(getFileNameWithoutExt(path) + fmt.Sprintf(".%s", *c.dest))
+			fmt.Println(getFileNameWithoutExt(path) + fmt.Sprintf(".%s", c.dest) + " を出力します")
+			out, err := os.Create(getFileNameWithoutExt(path) + fmt.Sprintf(".%s", c.dest))
 			defer out.Close()
 
 			// 変換後拡張子に応じたエンコード
-			switch *c.dest {
+			switch c.dest {
 			case "jpg", "jpeg":
 				jpeg.Encode(out, img, nil)
 
@@ -65,7 +62,7 @@ func Convert(args []string) error {
 				gif.Encode(out, img, nil)
 			}
 
-			fmt.Println(getFileNameWithoutExt(path) + fmt.Sprintf(".%s", *c.dest) + " を出力しました")
+			fmt.Println(getFileNameWithoutExt(path) + fmt.Sprintf(".%s", c.dest) + " を出力しました")
 
 		}
 		return nil
@@ -79,4 +76,25 @@ func Convert(args []string) error {
 // getFileNameWithoutExt は拡張子を外したファイル名を返却する関数
 func getFileNameWithoutExt(path string) string {
 	return filepath.Base(path[:len(path)-len(filepath.Ext(path))])
+}
+
+func parseFlag(args []string) Cmdoption {
+
+	fmt.Println("parseFlagメソッド内のargsの値")
+	fmt.Println(args)
+
+	flg := flag.NewFlagSet(args[0], flag.ExitOnError)
+
+	c := Cmdoption{}
+	c.targetdir = *flg.String("t", "./", "Message")
+	c.src = *flg.String("s", "jpg", "Message")
+	c.dest = *flg.String("d", "png", "Message")
+
+	flg.Parse(args)
+
+	fmt.Println("parseFlagメソッド内のflgの値(flg.Parse後)")
+	fmt.Println(flg.Args())
+	fmt.Println(c.dest)
+
+	return c
 }
--- imgconv_test.go
+++ imgconv_test.go
@@ -1,6 +1,32 @@
 package imgconv
 
-import "testing"
+import (
+	"fmt"
+	"strings"
+	"testing"
+)
+
+var table = [...]string{
+	"imgconv",
+	"imgconv,-s,png,-d,jpg",
+	"imgconv,-s,jpg,-d,gif",
+}
+
+func TestConvert(t *testing.T) {
+
+	for i, test := range table {
+		args := strings.Split(test, ",")
+
+		fmt.Println("%i 回目のテストを実施します", i)
+		fmt.Println(args)
+		err := Convert(args)
+
+		if err != nil {
+			fmt.Println("処理中にエラーが発生しました。テスト失敗です。")
+		}
+	}
+
+}
 
 // getFileNameWithoutExt のテスト
 func TestGetFileNameWithoutExt(t *testing.T) {
